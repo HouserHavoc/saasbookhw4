e6076f8a242d12df16a498ebf55e5fbd83394e67
saasbook <saasbook@saasbook.(none)>
2014-11-24 02:12:14 -0400

    Finished

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -16,14 +16,14 @@ module NavigationHelpers
     when /^the home\s?page$/
       movies_path
 
-    when /^the edit page for "([^"]+)"$/
-      edit_movie_path(Movie.find_by_title($1).id)
+    when /^the edit page for "(.*)"/
+      edit_movie_path(Movie.find_by_title($1))
 
-    when /^the details page for "([^"]+)"$/
-      movie_path(Movie.find_by_title($1).id)
+    when /the details page for "(.*)"$/
+      movie_path(Movie.find_by_title($1))
 
-    when /^the Similar Movies page for "([^"]+)"$/
-      @movie_id = Movie.find_by_title($1).id
+    when /^the Similar Movies page for "(.*)"$/
+      @movie_id = Movie.find_by_title($1)
       movie_similar_path(@movie_id)
 
     when /^the RottenPotatoes home page$/
